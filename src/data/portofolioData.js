7fd4c520448f5ad12a2f7e3859dc49ac1117ba51
bangdastin <144580668+bangdastin@users.noreply.github.com>
2026-06-09 17:03:15 +0700

    Update deskripsi pekerjaan GP di portofolioData.js

--- src/data/portofolioData.js
+++ src/data/portofolioData.js
@@ -4,7 +4,7 @@ export const profileData = {
   address: "Medan, Jln Eka Rasmi Gg Eka Mulia No.21",
   links: "LinkedIn & GitHub: @lf321003",
   about: "I am a passionate Information Technology fresh graduate from IT Del, enthusiastic about cutting-edge technology. I have a special interest in software development, emphasizing problem-solving and creativity. Committed to continuous learning and skill development in the field of information technology.",
-  education: "Institut Teknologi Del - Sitoluama, Laguboti, Toba (Aug 2021-Sep 2024) | GPA: 3.12/4.00"
+  education: "Institut Teknologi Del - Sitoluama, Laguboti, Toba (Aug 2021-Sep 2024) | GPA: 3.18/4.00"
 };
 
 export const skillsData = [
@@ -21,55 +21,62 @@ export const skillsData = [
 export const projectsData = [
   {
     id: 1,
-    title: "Pega Developer - PT Asuransi SinarMas",
-    period: "Mar 2024-Present",
+    title: "SoftWare Engineer - PT Asuransi SinarMas",
+    period: "Mar 2024-Mar 2026",
     description: "Developed and maintained enterprise applications using Pega Platform, following PRPC best practices. Designed case types, UI forms, data models, and automated business logic.",
     techStack: ["Pega Platform", "PRPC"]
   },
-  {
+{
     id: 2,
+    title: "Technical Consultant - PT Artha Infotama",
+    period: "May 2026-Present",
+    description: "Backend architecture and systems integration specialist. Focused on strategic customization of Microsoft Dynamics GP to optimize ERP efficiency, build seamless cross-platform integrations, and deliver reliable software solutions for enterprise-level operational needs.",
+    techStack: ["Pega Platform", "PRPC"]
+  },
+  {
+    id: 3,
     title: "Full Stack Developer - SMAN2 LintongNihuta",
     period: "Project",
     description: "Developed a web-based student registration system to streamline the admission process. Designed responsive UIs and backend APIs for data submission and validation.",
     techStack: ["React.js", "Node.js"]
   },
   {
-    id: 3,
+    id: 4,
     title: "COVID-19 Tracking Vaccine Project - IT Del",
     period: "Feb 2023 - Jun 2023",
     description: "Led the design and implementation of a web-based system using .NET Core Framework to monitor COVID-19 vaccinations. Employed OOP principles like encapsulation and polymorphism.",
     techStack: [".NET Core", "C#", "OOP"]
   },
   {
-    id: 4,
+    id: 5,
     title: "Final Project II (Numero Sada Futsal) - IT Del",
     period: "Jan 2023-Jun 2023",
     description: "Developed a mobile application utilizing the Flutter framework for managing futsal activities, alongside a website built using the Laravel framework.",
     techStack: ["Flutter", "Laravel"]
   },
   {
-    id: 5,
+    id: 6,
     title: "BAAK Project - IT Del",
     period: "Dec 2023 - Jan 2024",
     description: "Built a website using Java programming language, Eclipse tools, and designed using HTML, CSS, and JavaScript.",
     techStack: ["Java", "HTML/CSS/JS"]
   },
   {
-    id: 6,
+    id: 7,
     title: "Applicaton Development Mobile (Koperasi) - IT Del",
     period: "Dec 2022-Jan 2023",
     description: "Developed a cooperative application using Dart programming language for mobile platforms.",
     techStack: ["Dart"]
   },
   {
-    id: 7,
+    id: 8,
     title: "OOP Project (Library Website) - IT Del",
     period: "Project",
     description: "Developed a library website using Java within NetBeans IDE, coupled with JavaFX for designing the visual layout.",
     techStack: ["Java", "JavaFX"]
   },
   {
-    id: 8,
+    id: 9,
     title: "Final Project I (Information System Canteen) - IT Del",
     period: "Jan 2022 - May 2022",
     description: "Conceptualized website interface using Figma and built functional aspects using PHP.",
